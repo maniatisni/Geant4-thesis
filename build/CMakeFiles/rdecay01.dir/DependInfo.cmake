cff671c967091b0fd40ec613304e4fc286fe0b01
nikos7ender <nikos7ender@gmail.com>
2019-04-16 18:15:34 +0300

    Fixed stuff

---
+++ build/CMakeFiles/rdecay01.dir/DependInfo.cmake
@@ -0,0 +1,65 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/rdecay01.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/rdecay01.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/ActionInitialization.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/ActionInitialization.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/DetectorConstruction.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/DetectorConstruction.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/EventAction.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/EventAction.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/EventMessenger.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/EventMessenger.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/HistoManager.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/HistoManager.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/PhysicsList.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/PhysicsList.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/PrimaryGeneratorAction.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/Run.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/Run.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/RunAction.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/RunAction.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/SteppingAction.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/SteppingAction.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/SteppingVerbose.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/SteppingVerbose.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/TrackingAction.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/TrackingAction.cc.o"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/src/TrackingMessenger.cc" "/home/nick/Desktop/Geant4-thesis/build/CMakeFiles/rdecay01.dir/src/TrackingMessenger.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/nick/geant4/geant4-install/include/Geant4"
+  "/home/nick/Desktop/Geant4-thesis/rdecay1/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
